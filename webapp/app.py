6d2e6f105d3f9f6302cbbb15aec249e120ab3596
chernhao <leechernhao123@gmail.com>
2023-11-15 14:31:24 +0800

    added mfa and change back commonpasswrd.txt

--- webapp/app.py
+++ webapp/app.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
-from werkzeug.security import check_password_hash
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'  # Change this to a random secret key
 
 # Function to check if the password meets the requirements
-def is_valid_password(password):
-    # Add your password requirements here (e.g., minimum length)
-    if len(password) < 8:
+def is_valid_password(password, mfa_enabled=False):
+    # Minimum length requirement
+    min_length = 10 if not mfa_enabled else 8
+    if len(password) < min_length:
         return False
 
     # Block common passwords from the list
@@ -16,6 +16,7 @@ def is_valid_password(password):
         return False
 
     # Add more password requirements as needed
+
     return True
 
 @app.route('/', methods=['GET', 'POST'])
@@ -23,8 +24,11 @@ def home():
     if request.method == 'POST':
         password = request.form['password']
 
+        # In a real application, you should check whether MFA is enabled for the user
+        mfa_enabled = False  # Replace with your MFA check logic
+
         # Validate the password
-        if not is_valid_password(password):
+        if not is_valid_password(password, mfa_enabled):
             flash('Invalid password. Please meet the password requirements.')
             return redirect(url_for('home'))
 
@@ -34,8 +38,12 @@ def home():
     # For GET requests or initial load, render the home page
     return render_template('home.html')
 
-@app.route('/welcome/<password>')
+@app.route('/welcome/<password>', methods=['GET', 'POST'])
 def welcome(password):
+    if request.method == 'POST':
+        # Add any logout logic here if needed
+        return redirect(url_for('home'))
+
     return render_template('welcome.html', password=password)
 
 if __name__ == '__main__':
